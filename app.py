a2c465a067e46a875fd7678d8db0711a1b07f460
Morgan Williams <morgachilla2018@gmail.com>
2025-03-25 16:01:05 -0500

    Testing Pytest again

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Small change to trigger CI/CD pipeline
-
-# Attempt to trigger pylint
-def better_code():
-    pass
-
 @app.route('/api/message', methods=['GET'])
 def get_message():
     return jsonify({'message': 'Hello from Flask!'}), 200
